b6e7e63088a47d47aa9324cfba76f62f6430d890
amalyarzky <152752288+amalyarzky@users.noreply.github.com>
2023-12-07 07:03:30 -0800

    Add files via upload

---
+++ register.php
@@ -0,0 +1,31 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Halaman Login</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+
+    <body>
+        <div class="container">
+        <h1>Register</h1>
+            <form>
+                <label>Username</label>
+                <br>
+                <input type="text">
+                <br>
+                <label>Email</label>
+                <br>
+                <input type="text">
+                <br>
+                <label>Password</label>
+                <br>
+                <input type="password">
+                <br>
+                <button>Register</button>
+                <p> Sudah punya akun?
+                  <a href="login.php">Login di sini</a>
+                </p>
+            </form>
+        </div>
+    </body>
+</html>
